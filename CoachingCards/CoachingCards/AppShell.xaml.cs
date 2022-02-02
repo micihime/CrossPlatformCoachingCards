6d7cd12a2ac7ac0f99e0d90a62bddc3acc91969c
A41317117 <A41317117@BE2AQ412>
2022-02-02 14:15:18 +0100

    Add project files.

---
+++ CoachingCards/CoachingCards/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CoachingCards.ViewModels;
+using CoachingCards.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CoachingCards
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ CoachingCards/CoachingCards/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CoachingCards.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CoachingCards.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
