b0153fb587e5aca180d91989d0342581efdc2795
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2022-02-08 19:24:42 +0100

    Different font sizes for games - test

--- CoachingCards/CoachingCards/Views/DeckPageRight.xaml.cs
+++ CoachingCards/CoachingCards/Views/DeckPageRight.xaml.cs
@@ -18,8 +18,8 @@ namespace CoachingCards.Views
         private const string separatorImage = "separator3.png";
 
         private static Image cardDeckBackground;
-        private static Image separator1;
-        private static Image separator2;
+        //private static Image separator1;
+        //private static Image separator2;
 
         private static Label cardHeading;
         private static Label cardText;
@@ -39,8 +39,8 @@ namespace CoachingCards.Views
             InitializeComponent();
 
             cardDeckBackground = CardDeckBackground;
-            separator1 = Separator1;
-            separator2 = Separator2;
+            //separator1 = Separator1;
+            //separator2 = Separator2;
 
             cardHeading = CardHeading;
             cardText = CardText;
@@ -56,15 +56,15 @@ namespace CoachingCards.Views
 
         private static void ResetGame()
         {
-            cards = CardList.GetNewDeckRightHemisphere();
+            cards = CardList.GetNewDeck();
 
             //animateCard(animShuffleDeck);
 
             showBack = true;
 
             cardDeckBackground.Source = backgroundImage; //cardDeck.BackgroundColor = Color.Orange;
-            separator1.Source = null;
-            separator2.Source = null;
+            //separator1.Source = null;
+            //separator2.Source = null;
 
             cardHeading.Text = empty;
             cardText.Text = empty;
@@ -103,8 +103,8 @@ namespace CoachingCards.Views
             showBack = false;
 
             cardDeckBackground.Source = cards[0].IsLeft ? backgroundImageLeft : backgroundImageRight; //cardDeck.BackgroundColor = Color.Lavender;
-            separator1.Source = separatorImage;
-            separator2.Source = separatorImage;
+            //separator1.Source = separatorImage;
+            //separator2.Source = separatorImage;
 
             cardHeading.Text = cards[0].Heading;
             cardText.Text = cards[0].Text;
@@ -119,8 +119,8 @@ namespace CoachingCards.Views
             showBack = true;
 
             cardDeckBackground.Source = backgroundImage; //cardDeck.BackgroundColor = Color.Orange;
-            separator1.Source = null;
-            separator2.Source = null;
+            //separator1.Source = null;
+            //separator2.Source = null;
 
             cardHeading.Text = empty;
             cardText.Text = empty;
@@ -130,8 +130,8 @@ namespace CoachingCards.Views
         private static void EmptyDeck()
         {
             cardDeckBackground.Source = emptyDeckBackgroundImage;  //cardDeck.BackgroundColor = Color.White;
-            separator1.Source = null;
-            separator2.Source = null;
+            //separator1.Source = null;
+            //separator2.Source = null;
 
             cardHeading.Text = empty;
             cardText.Text = empty;
