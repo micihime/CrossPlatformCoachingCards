0485aaaeda7cb9167c9284e8f8e01be31e58b750
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2022-05-25 15:47:46 +0200

    Temporary modification of the messages in order to debug better

--- CoachingCards/CoachingCards/ViewModels/SubscribeViewModel.cs
+++ CoachingCards/CoachingCards/ViewModels/SubscribeViewModel.cs
@@ -18,8 +18,7 @@ namespace CoachingCards.ViewModels
         private const string apiEndpoint = "/api/v2/subscribers";
         private const string apiMediaType = "application/json";
         private const string apiTokenKey = "x-mailerlite-apikey";
-        private const string apiToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiYzg2NDM3Y2Q4MDYwOTU0NGY1YjVmMjBlMDI3NjMwNTc5ZTExZTYyZmU1ZDA1MDU2ZmJjYzIxZGNmZjc0NDkyZGFlODNiOTFiZWI1N2ZmMjgiLCJpYXQiOjE2NTI2OTE2MDEuNDkwMTI1LCJuYmYiOjE2NTI2OTE2MDEuNDkwMTMsImV4cCI6NDgwODM2NTIwMS40ODQyMzMsInN1YiI6IjU2ODM4Iiwic2NvcGVzIjpbXX0.tyRAyXul_26ps_dwHAkWQYaf5DBHQ6OH9KiSbIgrZTo5DiByU1uSiG70jrbJbd206LAHcf4gQkyNtteiTUtvGUqNqnq_luX5F5IWyuNLYWBewKOw2J-u1-qpKuS-STZXp22yOXGhFr5nPcfM7gu2JAUNGIrKmKUG8qIY9TivtrCv1wCadAPGprnp4jCU-kgNg2s4a33AISKNNCGhPFQ3B_nrtjDm4JtAV3UyxeZjU4vVaObXx3nBKF_--T8EE-rf2PIW2bgRtV0Vj8NioP66YohY9RZmaLWFGaKpkGILp2OghvJfP2WzySJ3myj-rjXjabcDrTAW7A9_3lY_XDHbvoLBbgfLVT290KH4nv0gkLLTmfL4D4V40ST_Gh2GHUQRj5TuOhzdoHnQnOMg8WDptfGFIWhTL6B2SkqdmXZMgSIoBndnT7PyetG3LSdImxsO4v9Ivn2gwDLu_Q6r7z2UvX73Z5meTXYNMGKyC0RG_iOdisYL5jKcOu-QEF8WqvHq0Dn5YBkfUpseeYoypes-FI2LM4gXrgLEAn_D-pilJhDXJHWh61NFu1kmpaEYJ5QG9XqZ4AystIHxxrL6kmQIHKzlg_9MBvSaL3R7X9aI4WxXYUUpXhU9eax24ug8tINbBG60lbRB4O-4mH04nvQATM-qsRghYCh_c38zR-vE6Xs";
-
+        private const string apiToken = "1a2bf25bdfc8ea00654ec4e9c7ef5fd5";
         private const string successHeading = "Děkujeme!";
         private const string successText = "Přihlášení k odběru novinek bylo úspěšné.";
         private const string failureHeading = "UPOZORNENI";
@@ -71,15 +70,16 @@ namespace CoachingCards.ViewModels
                 var content = new StringContent(json.ToString(), Encoding.UTF8, apiMediaType);
 
                 var response = client.PostAsync(apiEndpoint, content).Result;
+                string message = $"Status code: {response.StatusCode}\nReason phrase: {response.ReasonPhrase}\nContent: {response.Content}\n";
 
                 if (response.IsSuccessStatusCode)
                     await App.Current.MainPage.DisplayAlert(successHeading, successText, buttonText);
                 else
-                    await App.Current.MainPage.DisplayAlert(failureHeading, failureText, buttonText);
+                    await App.Current.MainPage.DisplayAlert(failureHeading, message, buttonText);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                await App.Current.MainPage.DisplayAlert(failureHeading, failureText, buttonText);
+                await App.Current.MainPage.DisplayAlert(failureHeading, e.Message + e.InnerException, buttonText);
             }
         }
     }
